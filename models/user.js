e70ba31146e29bd6617e4c3cc5e49e6676f85953
Shailendra Srivastava <sobhit6548@gmail.com>
2021-05-26 11:59:51 +0530

    addes message notification system

--- models/user.js
+++ models/user.js
@@ -1,24 +1,28 @@
-const mongoose = require('mongoose');
-const passportLocalMongoose = require('passport-local-mongoose');
-const Product = require('./product');
-
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+const Product = require("./product");
 
 const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true,
-        unique:true
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  number: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  cart: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
     },
-    cart: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref:'Product'
-        }
-    ]
-})
+  ],
+});
 
 userSchema.plugin(passportLocalMongoose);
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
--- routes/auth.js
+++ routes/auth.js
@@ -1,51 +1,75 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/user');
-const passport = require('passport');
+const User = require("../models/user");
+const passport = require("passport");
 
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+
+//   .then((message) => console.log(message.sid));
+const client = require("twilio")(accountSid, authToken);
+let val;
 // Get the signup form
-router.get('/register', async (req, res) => {
-    res.render('auth/signup');
-})
-
-router.post('/register', async (req, res) => {
-    
-    try {
-        const user = new User({ username: req.body.username, email: req.body.email });
-        const newUser = await User.register(user, req.body.password);
-        req.flash('success', 'Registered Successfully,Please Login to Continue');
-        res.redirect('/login');
-    }
-    catch (e) {
-        req.flash('error', e.message);
-        res.redirect('/register')
-    }
+router.get("/register", async (req, res) => {
+  res.render("auth/signup");
 });
 
+router.post("/register", async (req, res) => {
+  try {
+    const user = new User({
+      username: req.body.username,
+      email: req.body.email,
+      number: req.body.number,
+    });
+    const newUser = await User.register(user, req.body.password);
+    req.flash("success", "Registered Successfully,Please Login to Continue");
+    val = `+91${req.body.number}`;
+    client.messages.create({
+      body: req.flash("success"),
+      from: "+12489491504",
+      to: `+91${req.body.number}`,
+    });
+    //   .then((message) => console.log(message.sid));
+    res.redirect("/login");
+  } catch (e) {
+    req.flash("error", e.message);
+    res.redirect("/register");
+  }
+});
 
 // Get the login form
-router.get('/login', async (req, res) => {
-    
-    res.render('auth/login')
-})
-
-router.post('/login',
-    passport.authenticate('local',
-        {
-            failureRedirect: '/login',
-            failureFlash: true
-        }
-    ), (req, res) => {
-        req.flash('success', `Welcome Back!! ${req.user.username}`)
-        res.redirect('/products');
+router.get("/login", async (req, res) => {
+  res.render("auth/login");
 });
 
-// Logout the user from the current session
-router.get('/logout', (req, res) => {
-    req.logout();
-    req.flash('success', 'Logged Out Successfully');
-    res.redirect('/login');
-})
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  (req, res) => {
+    req.flash("success", `Welcome Back!! ${req.user.username}`);
+    client.messages.create({
+      body: `Welcome Back!! ${req.user.username}`,
+      from: "+12489491504",
+      to: `+91${req.user.number}`,
+    });
+    //   .then((message) => console.log(message.sid));
+    res.redirect("/products");
+  }
+);
 
+// Logout the user from the current session
+router.get("/logout", (req, res) => {
+  req.flash("success", "Logged Out Successfully");
+  client.messages.create({
+    body: req.flash("success"),
+    from: "+12489491504",
+    to: `+91${req.user.number}`,
+  });
+  req.logout();
+  res.redirect("/login");
+});
 
 module.exports = router;
